ca1a006236a5a4c92cce1559eea7251f3318e4a5
RanolP <RanolP@users.noreply.github.com>
2020-05-24 16:27:11 +0000

    deploy: db9c55e67de9a20bb9f5cfa3e6d4d6095d34c5d5

--- frontend.js
+++ frontend.js
@@ -504,7 +504,7 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper1472 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper1470 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 328, __wbg_adapter_18);
         return addHeapObject(ret);
     };
